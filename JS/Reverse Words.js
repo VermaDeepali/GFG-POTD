878344240777e0c918e6f6e639e05d96306f7a88
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2024-09-19 22:17:07 +0530

    Create Reverse Words.js

---
+++ JS/Reverse Words.js
@@ -0,0 +1,29 @@
+// Given a String str, reverse the string without reversing its individual words. Words are separated by dots.
+
+// Note: The last character has not been '.'. 
+
+// Examples :
+
+// Input: str = i.like.this.program.very.much
+// Output: much.very.program.this.like.i
+// Explanation: After reversing the whole string(not individual words), the input string becomes much.very.program.this.like.i
+
+// Input: str = pqr.mno
+// Output: mno.pqr
+// Explanation: After reversing the whole string , the input string becomes mno.pqr
+
+// Expected Time Complexity: O(|str|)
+// Expected Auxiliary Space: O(|str|)
+
+// Constraints:
+// 1 <= |str| <= 105
+
+
+class Solution {
+    // Function to reverse words in a given string.
+    reverseWords(str) {
+        // code here
+        return str.split('.').reverse().join('.');
+        
+    }
+}
