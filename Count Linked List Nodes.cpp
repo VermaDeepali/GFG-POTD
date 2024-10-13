68107923cabcb9fe5cdee515c32e12a45e9ed669
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2024-10-14 00:23:06 +0530

    Create Count Linked List Nodes.cpp

---
+++ Count Linked List Nodes.cpp
@@ -0,0 +1,34 @@
+// Given a singly linked list. The task is to find the length of the linked list, where length is defined as the number of nodes in the linked list.
+
+// Examples :
+
+// Input: LinkedList : 1->2->3->4->5
+
+// Output: 5
+// Explanation: Count of nodes in the linked list is 5, which is its length.
+// Input: LinkedList : 2->4->6->7->5->1->0
+ 
+// Output: 7
+// Explanation: Count of nodes in the linked list is 7. Hence, the output is 7.
+// Expected Time Complexity: O(n)
+// Expected Auxilliary Space: O(1)
+
+// Constraints:
+// 1 <= number of nodes <= 105
+// 1 <= node->data <= 103
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    int getCount(struct Node* head) {
+
+        // Code here
+        Node *ptr = head;
+        int count = 0;
+        while(ptr!=NULL){
+            count++;
+            ptr=ptr->next;
+        }
+        return count;
+    }
+};
