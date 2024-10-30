12d4d080b64d0964169925ecb69f0f7825ae0efd
Deepali Verma <41052882+VermaDeepali@users.noreply.github.com>
2024-10-30 21:59:28 +0530

    Create Pairs with difference k.cpp

---
+++ Pairs with difference k.cpp
@@ -0,0 +1,36 @@
+// Given an array arr[] of positive integers. Find the number of pairs of integers whose difference equals a given number k.
+// Note: (a, b) and (b, a) are considered the same. Also, the same numbers at different indices are considered different.
+
+// Examples:
+
+// Input: arr[] = [1, 5, 3, 4, 2], k = 3
+// Output: 2
+// Explanation: There are 2 pairs with difference 3,the pairs are {1, 4} and {5, 2} 
+// Input: arr[] = [8, 12, 16, 4, 0, 20], k = 4
+// Output: 5
+// Explanation: There are 5 pairs with difference 4, the pairs are {0, 4}, {4, 8}, {8, 12}, {12, 16} and {16, 20}.
+// Constraints:
+// 1 <= arr.size() <= 106
+// 1 <= k <= 106
+// 0 <= arr[i] <= 106
+
+class Solution {
+  public:
+    /* Returns count of pairs with difference k  */
+    int countPairsWithDiffK(vector<int>& arr, int k) {
+        // code here
+        map<int,int> m;
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+         
+            if(m.find(arr[i]-k)!=m.end()){
+                count+=m[arr[i]-k];
+            }
+            if(m.find(k+arr[i])!=m.end()){
+                 count+=m[arr[i]+k];
+            }
+               m[arr[i]]++;
+        }
+        return count;
+    }
+};
